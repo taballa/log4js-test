8b40358795ef16b47c10e7d344368e8ac58c4d06
TinkPad <taballa@gmail.com>
2014-01-14 16:32:13 +0800

    append log4js

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var log4js = require('log4js')
+log4js.configure({
+    appender: [
+        {type: 'console'},
+        {
+            type: 'file',
+            filename: 'log/access.log',
+            maxLogSize: 1024,
+            backups: 3,
+            category: 'normal'
+        }
+    ],
+    replaceConsole: true
+})
+
+var logger = log4js.getLogger('normal');
+logger.setLevel('INFO')
+
 
 var app = express();
 
